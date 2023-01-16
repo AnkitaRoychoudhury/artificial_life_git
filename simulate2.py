11da9345ca5edc508409eb02b674b62fee32394c
AnkitaRoychoudhury <aroychou@caltech.edu>
2023-01-15 21:52:00 -0600

    motors

--- simulate2.py
+++ simulate2.py
@@ -3,8 +3,9 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
+import random
 
-num_iters = 100
+num_iters = 10000
 physicsClient = p.connect(p.GUI)
 
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -23,6 +24,11 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(num_iters)
 frontLegSensorValues = np.zeros(num_iters)
 
+a = -np.pi/2
+b = np.pi/2
+targPos = (b-a) * random.random() + a
+#targPos = random.uniform(a,b)
+force = 150
 
 for i in range(num_iters):
 	p.stepSimulation()
@@ -31,8 +37,15 @@ for i in range(num_iters):
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 	#print(backLegTouch)
+
+	a = -np.pi/2
+	b = np.pi/2
+	targPos = (b-a) * random.random() + a		
 	pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,jointName = "Torso_BackLeg", 
-	controlMode = p.POSITION_CONTROL,targetPosition = 0.0,maxForce = 500)
+	controlMode = p.POSITION_CONTROL,targetPosition = -targPos ,maxForce = force)
+
+	pyrosim.Set_Motor_For_Joint(bodyIndex = robotId,jointName = "Torso_FrontLeg", 
+	controlMode = p.POSITION_CONTROL,targetPosition = targPos ,maxForce = force)
 
 	time.sleep(1/30)
 	#print(i)
