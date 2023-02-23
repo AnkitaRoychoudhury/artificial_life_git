b85b007b08b5b60463914be1854bbaca42d897db
AnkitaRoychoudhury <aroychou@caltech.edu>
2023-02-23 11:29:35 -0600

    a6

--- search.py
+++ search.py
@@ -2,10 +2,10 @@ import os
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
 phc = PARALLEL_HILL_CLIMBER()
-for i in range(7):
+#for i in range(0):
     #phc.Show_Best()
-    phc.Evolve()
-    phc.Show_Best()
+phc.Evolve()
+phc.Show_Best()
 
 #os.system('python3 hillclimber.py')
 # os.system('python3 simulate2.py')
